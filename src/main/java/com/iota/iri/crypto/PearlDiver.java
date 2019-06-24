6e2598a8b2063aa7db67afed9bc58c83a9502d11
tylerw1369 <39392335+tylerw1369@users.noreply.github.com>
2019-06-23 21:40:04 -0600

    Update PearlDiver.java

--- src/main/java/com/iota/iri/crypto/PearlDiver.java
+++ src/main/java/com/iota/iri/crypto/PearlDiver.java
@@ -2,6 +2,9 @@ package com.iota.iri.crypto;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.dltcollab.Dcurl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Proof of Work calculator.
@@ -21,6 +24,7 @@ public class PearlDiver {
         COMPLETED
     }
 
+    private static final Logger log = LoggerFactory.getLogger(PearlDiver.class);
     private static final int TRANSACTION_LENGTH = 8019;
 
     private static final int CURL_HASH_LENGTH = 243;
@@ -34,32 +38,39 @@ public class PearlDiver {
 
     private static boolean isExternal = false;
 
-     public static void init(String exlibName) {
+    public static void init(String exlibName) {
         try {
-            System.loadLibrary(exlibName);
+            try {
+                System.loadLibrary(exlibName);
+            } catch (java.lang.UnsatisfiedLinkError e) {
+                if (exlibName.equals("dcurl")) {
+                    Dcurl dcurl = new Dcurl();
+                    dcurl.loadLibraryFromJar();
+                }
+            }
             if (PearlDiver.exlibInit()) {
                 isExternal = true;
             }
         } catch (java.lang.UnsatisfiedLinkError e) {
-            /* Do Nothing */
+            log.info("No external library for {}", PearlDiver.class.getSimpleName());
         } catch (java.lang.NullPointerException e) {
-            /* Do Nothing */
+            log.info("The external library name is an empty string");
         }
     }
 
-     /* Initialization function of external pow library */
+    /* Initialization function of external pow library */
     private static native boolean exlibInit();
 
-     /* Search function of external pow library */
+    /* Search function of external pow library */
     private static native boolean exlibSearch(final byte[] transactionTrits, final int minWeigtMagnitude, int numberOfThreads);
 
-     /* Cancel function of external pow library */
+    /* Cancel function of external pow library */
     private static native void exlibCancel();
 
-     /* Destroy function of external pow library */
+    /* Destroy function of external pow library */
     public static native void exlibDestroy();
 
-     public static void destroy() {
+    public static void destroy() {
         if (isExternal) {
             PearlDiver.exlibDestroy();
         }
@@ -77,17 +88,18 @@ public class PearlDiver {
      * @return <tt>true</tt> if search completed successfully.
      * the nonce will be written to the end of {@code transactionTrits}
      */
-    public synchronized boolean search(final byte[] transactionTrits, final int minWeightMagnitude, int numberOfThreads) {
-
+    public boolean search(final byte[] transactionTrits, final int minWeightMagnitude,
+                          int numberOfThreads) {
         validateParameters(transactionTrits, minWeightMagnitude);
-            if (isExternal) {
-            return PearlDiver.exlibSearch(transactionTrits, minWeightMagnitude, int numberOfThreads);
-            } else {
+        if (isExternal) {
+            return PearlDiver.exlibSearch(transactionTrits, minWeightMagnitude, numberOfThreads);
+        } else {
             return isearch(transactionTrits, minWeightMagnitude, numberOfThreads);
         }
     }
 
-     public synchronized boolean isearch(final byte[] transactionTrits, final int minWeightMagnitude, int numberOfThreads) {
+    public synchronized boolean isearch(final byte[] transactionTrits, final int minWeightMagnitude,
+                                        int numberOfThreads) {
         synchronized (syncObj) {
             state = State.RUNNING;
         }
@@ -141,6 +153,7 @@ public class PearlDiver {
             throw new RuntimeException(
                     "Invalid transaction trits length: " + transactionTrits.length);
         }
+
         if (minWeightMagnitude < 0 || minWeightMagnitude > CURL_HASH_LENGTH) {
             throw new RuntimeException("Invalid min weight magnitude: " + minWeightMagnitude);
         }
